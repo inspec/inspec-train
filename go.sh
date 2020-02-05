e929b8e39d44dfbe99b5d97d790e727a2cf6663b
Ryan Davis <ryand-ruby@zenspider.com>
2020-02-05 15:59:13 -0800

    Clean up go.sh based on shellcheck feedback.
    Signed-off-by: Ryan Davis <zenspider@chef.io>

--- go.sh
+++ go.sh
@@ -2,19 +2,24 @@
 
 set -e
 
-if [ -n "${UPDATE:-}" -o \! -d vendor/bundle ]; then
-    bundle config --local path $PWD/vendor/bundle
+echo "--- bundle"
+
+if [ -n "${UPDATE:-}" ] || [ ! -d vendor/bundle ]; then
+    bundle config --local path "$PWD/vendor/bundle"
     bundle install --jobs 7 --retry 3
 fi
 
 IN=$(bundle info --path inspec)
-GEM=$PWD/Gemfile
+GEM="$PWD/Gemfile"
+
+echo "+++ run"
 
 (
-    cd $IN
+    cd "$IN"
     export SLOW=1
     export NO_AWS=1
     export CHEF_LICENSE=accept-no-persist
 
-    bundle exec --gemfile=$GEM rake ${RAKE_TASK:-test:unit}
+    # shellcheck disable=SC2086
+    bundle exec --gemfile="$GEM" rake ${RAKE_TASK:-test:unit}
 )
